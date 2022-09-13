153b83854bcd8020026898384f3eb5e93a76f7e0
Vira Murray <vira.avramenko@gmail.com>
2022-09-13 13:47:29 -0400

    "Methods exercise 2 initial commit with soluttion

---
+++ Method.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise_2
+{
+    internal class Method
+    {
+        public static int OperatorAdd(int x, int y)
+        {
+            return x + y;
+        }
+        public static int OperatorSubstract(int x, int y)
+        {
+            return x - y;
+        }
+        
+        public static int OperatorMultiply(int x, int y)
+        {
+            return (x * y);
+        }
+        public static int OperatorDevide(int x, int y) 
+        { 
+            if (y == 0)
+            {
+                return 0;
+            }
+            return x/y;
+        }
+
+        public static int OperatorModulo(int x, int y)
+        {
+            return (x%y);
+        }
+
+        public static int OperatorSum(params int[] parameters) //integer array here is called "parameters"
+        {
+            int sum = 0;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                sum += parameters[i];
+            }
+            return sum;
+        }
+
+    }
+}
+
+
+   
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Methods_Exercise_2;
+Console.WriteLine("Add:");
+Console.WriteLine(Method.OperatorAdd(2,2));
+Console.WriteLine("Substract:");
+Console.WriteLine(Method.OperatorSubstract(2, 2));
+Console.WriteLine("Multiply:");
+Console.WriteLine(Method.OperatorMultiply(2, 2));
+Console.WriteLine("Devide:");
+Console.WriteLine(Method.OperatorDevide(2, 2));
+Console.WriteLine("Modulus:");
+Console.WriteLine(Method.OperatorModulo(3,2));
+Console.WriteLine("Params:");
+Console.WriteLine(Method.OperatorSum(4,77,34,987,2,4,5,6));
